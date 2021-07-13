78471c5193f38420c5a74f6fc3fdb576c420da25
Ranieri Lucas <rlmdantas15@gmail.com>
2021-07-13 14:13:36 -0300

    aula 01 - projeto criado e conectado ao firebase

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from './components/Button';
+
+function App() {
+  return (
+    <div>
+      <a href="htttps://www.google.com.br" target="_self">Clique aqui</a>
+      <Button text="agora já era a goiaba"/>
+      
+    </div>
+    
+  );
+}
+
+export default App;
